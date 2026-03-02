74e9617b4194a3670c1d367b3296a6948eac1142
suriya-a9 <uriyaprakash@gmail.com>
2026-03-02 19:07:29 +0530

    after implemented seo to a page

--- app/testing/page.js
+++ app/testing/page.js
@@ -2,6 +2,25 @@ import dbConnect from "../../lib/db";
 import Testing from "../../model/testing";
 import TestClient from "./testClient";
 
+export const metadata = {
+    title: "Testing Page",
+    description: "View and manage testing items.",
+    keywords: ["testing", "nextjs", "crud", "demo"],
+    openGraph: {
+        title: "Testing Page",
+        description: "View and manage testing items.",
+        url: "http://localhost:3000/testing",
+        images: [
+            {
+                url: "http://localhost:3000/og-image.png",
+                width: 800,
+                height: 600,
+                alt: "Testing Page",
+            },
+        ],
+    },
+};
+
 export default async function TestServer() {
     await dbConnect();
     const tests = await Testing.find().lean();
